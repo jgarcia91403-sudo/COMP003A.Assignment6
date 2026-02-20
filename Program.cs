dcf0103ebe22cb75879bfae8dd0207fa43c04c98
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-20 11:56:58 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003A.Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("")
+                Book book2 = new Book("")
+                Book book3 = new Book("")
+                bool exitProgram = false;
+            while (!exitProgram)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1. Display all books");
+                Console.WriteLine("2. update one book");
+                Console.WriteLine("3. Exite");
+                Console.
+            }
+        }
+    }
+}
